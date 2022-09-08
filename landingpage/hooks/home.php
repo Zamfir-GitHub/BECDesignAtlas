859e9547b2bd3c60c25a0d78f5f11edde0b01341
MonsterScope_Zamfir <zamfir@monsterscopestudios.com>
2022-09-09 02:39:13 +1000

    Created the plugin!

---
+++ landingpage/hooks/home.php
@@ -0,0 +1,79 @@
+<?php
+
+
+
+function HookLandingPageHomeReplacehome()
+{
+	echo "<h> Hello there! This is a WIP </h>";
+
+	return true;
+}
+
+
+/*
+
+function HookLandingPageHomeReplaceslideshow(){
+
+}
+
+function HookLandingPageHomeHomereplacewelcome(){
+
+}
+
+
+//homeheader
+function HookLandingPageHomehomeheader()
+{
+    ?>
+
+<!-- Header -->
+<header class="header header-fixed-on-mobile" data-sticky-threshold="0" data-bkg-threshold="100">
+				<div class="header-inner">
+					<div class="row nav-bar">
+						<div class="column width-14 nav-bar-inner">
+							<div class="logo">
+								<div class="logo-inner">
+									<a href="index.html"><img src="design_atlas_assets/LOGO/DesignAtlas Logo.png" alt="Design Atlas Logo" /></a>
+									<a href="index.html"><img src="design_atlas_assets/LOGO/DesignAtlas Favicon/DesignAtlas-Favicon-1.png" alt="Design Atlas Logo" /></a>
+								</div>
+							</div>
+							
+								<!-- Main Menu  -->
+								<nav class="navigation nav-block primary-navigation nav-right">
+									<ul>
+										<li >
+											<a href="https://www.becdesignatlas.com.au/pages/home.php" >
+												Dash</a>
+										</li>
+										<li >
+											<a href="https://www.becdesignatlas.com.au/pages/search.php?search=collection:Playing Now">
+												Now Playing</a>
+										</li>
+										<li >
+											<a href="https://www.becdesignatlas.com.au/pages/search.php?search=collection:Projects">
+												Projects</a>
+										</li>
+										<li >
+											<a href="https://www.becdesignatlas.com.au/pages/search.php?search=collection:Industry News" >
+												Industry News</a>
+											
+										</li>
+										<li >
+											<a href="https://www.becdesignatlas.com.au/pages/search.php?search=collection:Products" >
+												Products</a>
+											
+										</li>
+										
+									</ul>
+								</nav> 
+						</div>
+					</div>
+				</div>
+			</header>
+			<!-- Header End -->
+
+    <?php
+}
+*/
+
+?>
